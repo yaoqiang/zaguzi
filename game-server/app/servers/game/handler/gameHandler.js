9437502f0a7c6f1db5bceb9f68d864b9a2dd74a1
yaoqiang <yaoqiangshmily@gmail.com>
2015-07-09 08:17:09 +0800

    return game type

--- game-server/app/servers/game/handler/gameHandler.js
+++ game-server/app/servers/game/handler/gameHandler.js
@@ -40,7 +40,7 @@ handler.join = function (msg, session, next) {
         session.set('gameId', data.gameId);
         session.pushAll();
 
-        next(null, {code: Code.OK, lobbyId: data.lobbyId, roomId: data.roomId, gameId: data.gameId, base: data.base, actors: data.actors});
+        next(null, {code: Code.OK, lobbyId: data.lobbyId, roomId: data.roomId, gameId: data.gameId, gameType: data.gameType, base: data.base, actors: data.actors});
     });
 
 };

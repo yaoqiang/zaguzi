328b37730198869ec9429e1da4bf840e59d1ad33
yaoqiang <qiang.yao@mtime.com>
2015-12-04 13:47:49 +0800

    task option

---
+++ game-server/app/domain/entity/task.js
@@ -0,0 +1,19 @@
+var _ = require('underscore');
+
+var tasks = require('../../../config/data/task');
+
+var taskUtil = exports.modules
+
+taskUtil.generateTasks = function(tasks, cb) {
+  if (_.isNull(tasks) {
+
+  }
+  else {
+    
+  }
+  cb(tasks);
+}
+
+taskUtil.updateTask = function(task, cb) {
+  cb(task);
+}

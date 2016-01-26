dac39ad83d7241488a24d1c47c3bc4e38250e8f5
yaoqiang <yaoqiangshmily@gmail.com>
2016-01-26 08:09:28 +0800

    财富榜, 股神榜脚本;

--- game-server/config/script/rankingTask.js
+++ game-server/config/script/rankingTask.js
@@ -8,26 +8,32 @@ var limit = 20;
 
 
 //财富榜
-db.player.aggregate([
-    { $project: { _id: 0, nickName: 1, avatar: 1, gold: 1, uid: 1, type: { $literal: "RICH" }, date:  { $literal: new Date() } }},
+var richRankingList = db.player.aggregate([
+    { $project: { _id: 0, nickName: 1, avatar: 1, gold: 1, uid: 1 }},
     { $match : { gold: { $gt: goldThreshold } } },
     { $sort:  { gold: -1 }},
-    { $limit: limit },
-    { $out: "rankingList" }
+    { $limit: limit }
 ]);
 
+rankingList.insert({ ranking: richRankingList._batch, type: "RICH", date: new Date() });
+
 //股神榜
-db.player.aggregate([
-    { $project: { _id: 0, nickName: 1, avatar: 1, uid: 1,
-    winning: {
-        $devide: ["$winNr", { $add: ["$winNr", "$loseNr"] }]
+var godRankingList = db.player.aggregate([
+    {
+        $project: {
+            _id: 0, nickName: 1, avatar: 1, uid: 1, winNr: 1, loseNr: 1,
+            battleCount: {$add: ["$winNr", "$loseNr"]},
+            winning: {
+                $divide: ["$winNr", {$cond: [{$eq: [{ $add: ["$winNr", "$loseNr"] }, 0]}, 1, {$add: ["$winNr", "$loseNr"]}]}]
+            }
+        }
     },
-    battleCount: { $add: ["$winNr", "$loseNr"] }, type: { $literal: "GOD" }, date:  { $literal: new Date() } }},
-    { $match : { battleCount: { $gt: battleThreshold } } },
-    { $sort:  { winning: -1 }},
-    { $limit: limit },
-    { $out: "rankingList" }
+    {$match: {battleCount: {$gt: battleThreshold}}},
+    {$sort: {winning: -1}},
+    {$limit: limit}
 ]);
+
+rankingList.insert({ ranking: godRankingList._batch, type: "GOD", date: new Date() });
 
 
 //昨日充值榜

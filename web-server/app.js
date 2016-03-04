adbe9c0cf0f95f91a27c854e3ccf44fad11a47af
yaoqiang <yaoqiangshmily@gmail.com>
2016-03-04 23:15:36 +0800

    remove debug log

--- web-server/app.js
+++ web-server/app.js
@@ -87,8 +87,6 @@ app.post('/loginByToken', function (req, res) {
         });
         return;
     }
-
-    console.log('token -> ', data.token);
     
     //result: [uid, timestamp, password]
     var result = Token.parse(data.token, secret);
@@ -98,8 +96,6 @@ app.post('/loginByToken', function (req, res) {
         });
         return;
     }
-
-    console.log('result -> ', result);
 
     var uid = result.uid, password = result.password;
 

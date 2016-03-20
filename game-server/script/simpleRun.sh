8e11c4fc6e39f9cf9e2c718cec75803a7483b50f
yaoqiang <yaoqiangshmily@gmail.com>
2016-03-20 15:27:18 +0800

    add simple deploy script

---
+++ game-server/script/simpleRun.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd zgz ; git pull ; pomelo stop ; pm2 kill ;
+pm2 start web-server/app.js ; cd game-server ; cnpm install ; pomelo start --daemon --env=production ;
+printf "deploy successful.\n"

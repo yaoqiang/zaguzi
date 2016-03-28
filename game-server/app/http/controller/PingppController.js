128b5e2e5b042f0addb07931ebce55f89e501a89
yaoqiang <yaoqiangshmily@gmail.com>
2016-03-28 09:10:01 +0800

    about payment bug fixed.

--- game-server/app/http/controller/PingppController.js
+++ game-server/app/http/controller/PingppController.js
@@ -27,30 +27,31 @@ module.exports = function (app) {
         // 异步通知
         try {
             // 验证 webhooks 签名
-            //var verifySignature = function (rawData, signature, pub_key_path) {
-            //    var verifier = crypto.createVerify('RSA-SHA256').update(rawData, "utf8");
-            //    var pub_key = fs.readFileSync(pub_key_path, "utf8");
-            //    return verifier.verify(pub_key, signature, 'base64');
-            //}
-            //
-            //// POST 原始请求数据是待验签数据，请根据实际情况获取
-            //var rawData = JSON.stringify(req.body);
-            //// 签名在头部信息的 x-pingplusplus-signature 字段
-            //var signature = req.headers['x-pingplusplus-signature'];
-            //// 请从 https://dashboard.pingxx.com 获取「Webhooks 验证 Ping++ 公钥」
-            //var pub_key_path = __dirname + "/../../../config/pingpp_rsa_public_key.pem";
-            //
-            //if (!verifySignature(rawData, signature, pub_key_path)) {
-            //    logger.error('verification failed');
-            //    res.sendStatus(400);
-            //    return;
-            //}
-            logger.info('verification succeeded for pingxx');
+            var verifySignature = function (rawData, signature, pub_key_path) {
+                var verifier = crypto.createVerify('RSA-SHA256').update(rawData, "utf8");
+                var pub_key = fs.readFileSync(pub_key_path, "utf8");
+                return verifier.verify(pub_key, signature, 'base64');
+            }
+
+            // POST 原始请求数据是待验签数据，请根据实际情况获取
+            var rawData = JSON.stringify(req.body);
+            // 签名在头部信息的 x-pingplusplus-signature 字段
+            var signature = req.headers['x-pingplusplus-signature'];
+            // 请从 https://dashboard.pingxx.com 获取「Webhooks 验证 Ping++ 公钥」
+            var pub_key_path = __dirname + "/../../../config/pingpp_rsa_public_key.pem";
+
+            if (!verifySignature(rawData, signature, pub_key_path)) {
+                logger.error('verification failed');
+                res.sendStatus(400);
+                return;
+            }
+            logger.debug('verification succeeded for pingxx');
 
             var result = req.body;
 
             switch (result.type) {
                 case "charge.succeeded":
+                    logger.debug('charge.succeeded..');
                     // 开发者在此处加入对支付异步通知的处理代码
                     res.sendStatus(200);
                     app.rpc.manager.universalRemote.payment4Pingpp(null, result.data.object, function () {

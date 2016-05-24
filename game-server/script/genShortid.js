792c76ea754cafca4b9ab2fe930a5c69a6b1bae3
yaoqiang <yaoqiangshmily@gmail.com>
2016-05-24 22:13:46 +0800

    update gen shortid script.

--- game-server/script/genShortid.js
+++ game-server/script/genShortid.js
@@ -2,6 +2,10 @@ var shortid = require('shortid');
 
 var mongojs = require('mongojs');
 
+var Promise = require('promise');
+
+var _ = require('lodash');
+
 var db = mongojs("mongodb://localhost:27017/zgz",
 ['user']);
 
@@ -10,10 +14,21 @@ var db = mongojs("mongodb://localhost:27017/zgz",
 db.user.find(function (err, docs) {
     if (err) return;
     if (docs != null && docs.length > 0) {
-        docs.forEach(function (user) {
-            if (user.shortid == null || user.shortid == undefined || user.shortid == '') {
-                db.user.update({_id: user._id}, {$set: {shortid: shortid.generate()}});
-            }
-        })
+        var updatePromiseList = _.map(docs, function (user) {
+            return new Promise(function (resolve, reject) {
+                db.user.update({_id: user._id}, {$unset: {shortid: shortid.generate()}}, function () {
+                    resolve();
+                });
+            })
+        });
+
+        Promise.all(updatePromiseList)
+            .then(function () {
+                console.log('update shortid finished...')
+                db.close();
+                process.exit();
+            })
+            .done();
+        
     }
 });

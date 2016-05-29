13a5a60ffb1f4c166e0cb21dc18a1d81bf24d990
yaoqiang <yaoqiangshmily@gmail.com>
2016-05-29 21:32:43 +0800

    shop conf  bug

--- game-server/app/services/paymentService.js
+++ game-server/app/services/paymentService.js
@@ -35,7 +35,7 @@ paymentService.requestChargesPingxx = function (data, cb) {
 
     data.productId = parseInt(data.productId);
     //
-    var product = _.findWhere(_.union(shopConf[device].gold, shopConf[device].prop), { id: data.productId });
+    var product = _.findWhere(_.union(shopConf[data.device].gold, shopConf[data.device].prop), { id: data.productId });
     if (product == undefined) {
         logger.debug('请求支付charge失败||%s||在服务器端没有找到该产品||%j', data.uid, { productId: data.productId, device: data.device, channel: data.channel });
         cb({ code: Code.FAIL });
@@ -108,7 +108,7 @@ paymentService.payment = function (order, charge, cb) {
 
     playerService.getUserCacheByUid(order.uid, function (user) {
 
-        var product = _.findWhere(_.union(shopConf[device].gold, shopConf[device].prop), { id: order.productId });
+        var product = _.findWhere(_.union(shopConf[order.device].gold, shopConf[order.device].prop), { id: order.productId });
         if (product == undefined) {
             logger.error("%j", {
                 uid: order.uid,

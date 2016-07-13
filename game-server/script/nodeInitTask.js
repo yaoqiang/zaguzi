11291f19615e605b47842e88ade96bc1711de7be
yaoqiang <qiang.yao@mtime.com>
2016-07-13 11:40:29 +0800

    处理新任务数据; 添加脚本readme

--- game-server/script/nodeInitTask.js
+++ game-server/script/nodeInitTask.js
@@ -1,1 +1,14 @@
-//nodejs执行, 重新初始化玩家任务数据. 为V1.3
+//nodejs执行, 重新初始化玩家任务数据. 为V1.3;
+
+var mongojs = require('mongojs');
+
+var db = require('../app/dao/mongodb');
+
+var taskUtil = require('../app/domain/entity/task');
+
+var taskList = taskUtil.initTasks();
+
+db.player.update({}, {$set: {tasks: taskList}}, {multi: true}, function() {
+
+})
+

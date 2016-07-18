af7f9d6de22ea3390bbddbaa67e41e44888830cf
yaoqiang <qiang.yao@mtime.com>
2016-07-18 17:31:17 +0800

    调整脚本

--- game-server/script/nodeRemoveDaily.js
+++ game-server/script/nodeRemoveDaily.js
@@ -11,21 +11,20 @@ var moment = require('moment');
 var now = moment();
 
 var lastMonth = moment(now).subtract(1,'months').endOf('month').format('YYYY-MM-DD HH:mm:ss');
-var lastSevenDay = moment(now).subtract(7, 'days').endOf('day').format('YYYY-MM-DD HH:mm:ss');
-
+var lastTwoWeek = moment(now).subtract(14, 'days').endOf('day').format('YYYY-MM-DD HH:mm:ss');
 
 
 new Promise(function (resolve, reject) {
     //删除上月的用户牌局记录
-    db.userBattleRecord.remove({createdAt: {$gt: lastMonth}}, function() {
-        console.log('remove last month userBattleRecord finished...');
+    db.userBattleRecord.remove({createdAt: {$lte: new Date(lastMonth)}}, function() {
+        console.log('remove userBattleRecord finished with the createdAt before last month...');
         resolve();  
     })
 })
 .then(function (resolve, reject) {
     //删除创建时间为7天前的数据, 用户日志只保留7天。
-    db.logUserRecord.remove({createdAt: {$lte: lastSevenDay}}, function() {
-        console.log('remove last 7 day logUserRecord finished...');
+    db.logUserRecord.remove({createdAt: {$lte: new Date(lastTwoWeek)}}, false, function() {
+        console.log('remove logUserRecord finished with the createdAt before last two weeks...');
         process.exit();
     })
 })

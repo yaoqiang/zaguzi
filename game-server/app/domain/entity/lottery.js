5038d5dd850820eccccc9921d04ef769f5c8f9c8
yaoqiang <yaoqiangshmily@gmail.com>
2016-09-03 10:27:45 +0800

    remove test code

--- game-server/app/domain/entity/lottery.js
+++ game-server/app/domain/entity/lottery.js
@@ -47,18 +47,18 @@ Lottery.lottery = function (orignalRates) {
 }
 
 Lottery.get = function () {
-    var orignalRates = [];
+    var originalRates = [];
     lotteryJson.forEach(function (item) {
         var probability = item.probability;
         if (probability < 0) {
             probability = 0;
         }
-        orignalRates.push(probability);
+        originalRates.push(probability);
     });
 
-    var orignalIndex = Lottery.lottery(orignalRates);
+    var originalIndex = Lottery.lottery(originalRates);
 
-    return lotteryJson[orignalIndex];
+    return lotteryJson[originalIndex];
 }
 
 // console.log(Lottery.get());
@@ -67,21 +67,21 @@ Lottery.get = function () {
 //-- for test
 //-----------------------
 
-var count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0, count7 = 0, count8 = 0;
-
-for (var i = 0; i < 10000000; i++) {
-    var lottery = Lottery.get();
-    if (lottery.id == 1) count1 += 1;
-    if (lottery.id == 2) count2 += 1;
-    if (lottery.id == 3) count3 += 1;
-    if (lottery.id == 4) count4 += 1;
-    if (lottery.id == 5) count5 += 1;
-    if (lottery.id == 6) count6 += 1;
-    if (lottery.id == 7) count7 += 1;
-    if (lottery.id == 8) count8 += 1;
-}
-
-console.log(count1, count2, count3, count4, count5, count6, count7, count8)
+//var count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0, count7 = 0, count8 = 0;
+//
+//for (var i = 0; i < 10000000; i++) {
+//    var lottery = Lottery.get();
+//    if (lottery.id == 1) count1 += 1;
+//    if (lottery.id == 2) count2 += 1;
+//    if (lottery.id == 3) count3 += 1;
+//    if (lottery.id == 4) count4 += 1;
+//    if (lottery.id == 5) count5 += 1;
+//    if (lottery.id == 6) count6 += 1;
+//    if (lottery.id == 7) count7 += 1;
+//    if (lottery.id == 8) count8 += 1;
+//}
+//
+//console.log(count1, count2, count3, count4, count5, count6, count7, count8)
 
 
 module.exports = Lottery;
